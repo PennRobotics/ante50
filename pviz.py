acd691acb3b143ef01147e3e20b0791533890051
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2023-03-01 14:44:54 +0100

    implement display fn and threads

--- pviz.py
+++ pviz.py
@@ -1,6 +1,7 @@
 # No idea if this will work, but here we go! (TODO)
 
 from enum import Enum, auto
+import threading
 
 class Gui(Enum):
     FILEIO = auto()
@@ -22,23 +23,54 @@ class Frontend:
     def __init__(self, mode, gh):
         match mode:
             case Gui.FILEIO:
-                print('f')
+                print('(file i/o)')
             case Gui.CONSOLE:
-                print('c')
+                print('(console)')
             case Gui.CURSES:
-                print('n')
+                import platform
+                match platform.system():
+                    case 'Windows':
+                        try:
+                            import curses
+                        except:
+                            raise ImportError('pip install windows-curses')
+                    case _:
+                        import curses
             case Gui.SDL2:
-                print('s')
+                import sdl2.ext
             case Gui.TKINTER:
-                print('t')
+                import tkinter
             case Gui.PYGAME:
-                print('p')
+                import pygame
             case _:
                 raise ValueError('unexpected case')
+        self.mode = mode
         # TODO: timed thread for display, thread for data
         # TODO: pass from one thread to other
         # TODO: how to wait for input in each method?
-        g.run()
+        self.disp_thread = threading.Thread(target=self.disp)
+        self.g_thread = threading.Thread(target=gh.run)
+        self.g_thread.start()
+        self.disp_thread.start()
+        while True:
+            pass  # TODO-debug
+
+    def disp(self):
+        match self.mode:
+            case Gui.FILEIO:
+                print('disp f')
+            case Gui.CONSOLE:
+                print('disp c')
+            case Gui.CURSES:
+                print('disp n')
+            case Gui.SDL2:
+                print('disp s')
+            case Gui.TKINTER:
+                print('disp t')
+            case Gui.PYGAME:
+                print('disp p')
+            case _:
+                raise ValueError('unexpected case')
 
 
 if __name__=='__main__':
